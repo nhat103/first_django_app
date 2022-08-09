ed3af6f74bef482100e16fafbfdbf4508bb01cf6
Nguyễn Nhật <ng.nhat103@gmail.com>
2022-08-09 14:24:58 +0700

    fist commit

---
+++ polls/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('datetime_published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+       # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.ImageField(default=0)  # default
+
+    def __str__(self):
+        return self.choice_text
+
+
+class User(models.Model):
+    username = models.TextField(max_length=32)
+    password = models.CharField(max_length=20)
